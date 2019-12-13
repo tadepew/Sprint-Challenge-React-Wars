a43946934c0aab8b045cefa10ca466437cf98b2b
Tristan Depew <tristan.depew@gmail.com>
2019-12-13 14:28:51 -0500

    tweaked styling

--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -1,13 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import { Button } from "reactstrap";
+
+const Button = styled.button`
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 3px;
+  border: 2px solid darkgrey;
+  color: #181816;
+  margin: 2 1em;
+  padding: 0.25em 1em;
+  margin-top: 15%;
+`;
 
 const PeopleCard = props => {
-  return (
-    <Button color="secondary" size="lg" marginTop="4%">
-      {props.name}
-    </Button>
-  );
+  return <Button>{props.name}</Button>;
 };
 
 export default PeopleCard;
